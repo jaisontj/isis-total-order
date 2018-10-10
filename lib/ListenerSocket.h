725bbee934a20d59448fc38fb9afbbd9c52f957a
Jaison Titus <jaison@myemail.com>
2018-10-10 15:42:38 -0400

    Moved implementation to SocketImpl to accomodate for TCP

--- lib/ListenerSocket.h
+++ lib/ListenerSocket.h
@@ -1,11 +1,11 @@
 #ifndef LISTENER_SOCKET_H
 #define LISTENER_SOCKET_H
 
-#include "DgramSocket.h"
+#include "SocketImpl.h"
 #include "NetworkDataTypes.h"
 #include "MessageHandler.h"
 
-class ListenerSocket: public DgramSocket {
+class ListenerSocket: public SocketImpl {
 	public:
 		ListenerSocket(std::string port);
 		void start_listening(MessageHandler &handler);
--- lib/SenderSocket.h
+++ lib/SenderSocket.h
@@ -1,9 +1,9 @@
 #ifndef SENDER_SOCKET_H
 #define SENDER_SOCKET_H
 
-#include "DgramSocket.h"
+#include "SocketImpl.h"
 
-class SenderSocket: public DgramSocket {
+class SenderSocket: public SocketImpl{
 	public:
 		SenderSocket(std::string hostname, std::string port);
 		int send(void *message, size_t message_size);
